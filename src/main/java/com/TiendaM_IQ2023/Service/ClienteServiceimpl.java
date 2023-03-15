92cdc9dde188c6be7390ea6a4d13d763f2f86441
sebgu <sebgu@Mycomputer>
2023-03-15 11:03:51 -0600

    TiendaM_IQ2023

---
+++ src/main/java/com/TiendaM_IQ2023/Service/ClienteServiceimpl.java
@@ -0,0 +1,42 @@
+package com.TiendaM_IQ2023.Service;
+
+import com.TiendaM_IQ2023.Dao.ClienteDao;
+import com.TiendaM_IQ2023.domain.Cliente;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class ClienteServiceimpl implements ClienteService {
+    
+    
+    @Autowired
+    ClienteDao clienteDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Cliente> getClientes() {
+        return (List<Cliente>) clienteDao.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Cliente getCliente(Cliente cliente) {
+        return clienteDao.findById(cliente.getIdcliente()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Cliente cliente) {
+        clienteDao.save(cliente);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Cliente cliente) {
+        clienteDao.deleteById(cliente.getIdcliente());
+    }
+    
+}
--- src/main/java/com/TiendaM_IQ2023/controller/IndexController.java
+++ src/main/java/com/TiendaM_IQ2023/controller/IndexController.java
@@ -1,40 +1,57 @@
 package com.TiendaM_IQ2023.controller;
 
+import com.TiendaM_IQ2023.Service.ClienteService;
 import com.TiendaM_IQ2023.domain.Cliente;
-import com.TiendaM_IQ2023.Dao.ClienteDao;
-import java.util.List;
-import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
 public class IndexController {
-    
+
     @Autowired
-    ClienteDao clienteDao;
-    
+    ClienteService clienteService;
+
     @GetMapping("/")
     public String inicio(Model model) {
-    //    log.info("Ahora desde MVC");
-    //    model.addAttribute("mensaje", "Hola desde el controller");
-        
-     //   Cliente cliente = new Cliente("Jonathan","Brenes Blanco","jbrenesbl@gmail.com","88745906");
-    //    model.addAttribute("objetocliente", cliente);
-        
-     //   Cliente cliente2 = new Cliente("Luisa","Castro Blanco","luisacastro533@gmail.com","69852195");
-     //   Cliente cliente3 = new Cliente("Carlos","Garcia Villalobos","villagarcia42@gmail.com","88793595");
-        
-     //   List<Cliente> clientes = Arrays.asList( cliente, cliente2, cliente3);
-     
-        var clientes = clienteDao.findAll();
+        log.info("Ahora desde MVC");
+
+        var clientes = clienteService.getClientes();
         model.addAttribute("clientes", clientes);
-        
-        return "Index"; 
-        
+
+        return "Index";
+
     }
     
+    @GetMapping("/nuevoCliente")
+    public String nuevoCliente(Cliente cliente){
+        return "modificarCliente";
+    }
+    
+    @PostMapping("/guardarCliente")
+    public String guardarCliente(Cliente cliente)
+    {
+        clienteService.save(cliente);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/modificarCliente/{Idcliente}")
+    public String modificarCliente(Cliente cliente, Model model)
+    {
+        cliente = clienteService.getCliente(cliente);
+        model.addAttribute("cliente", cliente);
+        return "modificarCliente";
+        
+    }
+    @GetMapping("/eliminarCliente/{Idcliente}")
+    public String eliminarCliente(Cliente cliente)
+    {
+        clienteService.delete(cliente);
+        return "redirect:/";
+    }
+
 }
